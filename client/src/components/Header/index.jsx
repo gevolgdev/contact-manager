36e6dbeb122bf20305d8390c773b7d5513a6a682
Pinelli <ricardosilvapinelli@gmail.com>
2023-03-24 21:13:31 -0300

    Alteraçoes Header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,12 +1,16 @@
-
+import IMAGE from '../../assets/imageHeader.png';
 
 export default function Header() {
 
+  const date = new Date();
 
   return (
-    <div className="flex flex-col w-full p-8 bg-slate-900 drop-shadow-lg">
-      <h1 className="text-4xl text-orange-500">Gerenciador de</h1>
-      <h1 className="text-4xl text-gray-400">contatos</h1>
+    <div className="flex flex-col w-full py-8 px-5">
+      <img src={IMAGE} className='w-[250px] mx-auto'/>
+      <span className='text-sm mb-3 text-gray-600'>Facilidade e agilidade</span>
+      <h1 className="text-5xl text-gray-900 font-bold">Gerenciador de contatos</h1>
+      <div>
+      </div>
     </div>
   )
-}
+};

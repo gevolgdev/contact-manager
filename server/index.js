df721ac52c0f14388334d6616381fbed1492b6fd
Pinelli <ricardosilvapinelli@gmail.com>
2023-03-27 15:55:49 -0300

    Put - Express

--- server/index.js
+++ server/index.js
@@ -13,6 +13,15 @@ const db = mysql.createPool({
   database: 'contact-schema',
 });
 
+app.get('/getContacts', (req, res) => {
+  let SQL = 'SELECT * FROM `contact-schema`.infos';
+
+  db.query(SQL, (err, result) => {
+    if (err) console.log(err)
+    else res.send(result)
+  });
+});
+
 app.post('/register', (req, res) => {
   const { name } = req.body;
   const { email } = req.body;
@@ -24,14 +33,20 @@ app.post('/register', (req, res) => {
   });
 });
 
-app.get('/getContacts', (req, res) => {
-  let SQL = 'SELECT * FROM `contact-schema`.infos';
+app.put('/edit', (req, res) => {
+  const { id } = req.body;
+  const { name } = req.body;
+  const { email } = req.body;
+  const { tel } = req.body;
 
-  db.query(SQL, (err, result) => {
-    if (err) console.log(err)
+  let SQL = 'UPDATE `contact-schema`.`infos` SET name = ?, email = ?, tel = ? WHERE id = ?'
+
+  db.query(SQL, [name, email, tel, id], (err, result)=> {
+    if(err) console.log(err)
     else res.send(result)
-  });
+  })
 });
+
 
 app.listen(3001, () => {
   console.log('Server is running...');
